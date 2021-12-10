5831fe2d1a38f2f86b1ac22b3c14c436d9bc8ef5
Igor <polygor@gmail.com>
2021-12-11 00:06:42 +0200

    debugs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-// App.js
 import './App.css';
 
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <h1>TEST</h1>
+    </div>
+  );
 }
 
 export default App;
